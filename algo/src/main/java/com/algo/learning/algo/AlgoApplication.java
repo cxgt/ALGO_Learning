88319908c48cf699aab30890524ed5d8449706d5
chenxin20 <chenxin20@asiainfo.com>
2024-06-12 16:50:32 +0800

    first commit

--- algo/src/main/java/com/algo/learning/algo/AlgoApplication.java
+++ algo/src/main/java/com/algo/learning/algo/AlgoApplication.java
@@ -1,7 +1,5 @@
 package com.algo.learning.algo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgoApplication {
